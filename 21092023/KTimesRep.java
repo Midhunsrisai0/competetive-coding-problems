4280ecc52274f7d85231b51a57bf8738eea3d83e
Midhunsrisai0 <21A81A0624@sves.org.in>
2023-09-21 11:57:55 +0530

    Added data

---
+++ 21092023/KTimesRep.java
@@ -0,0 +1,26 @@
+import java.util.LinkedHashMap;
+import java.util.TreeSet;
+
+class Main {
+    public static void main(String[] args) {
+        TreeSet<Integer> ts = new TreeSet<Integer>();
+        int nums[] = { 3, 1, 3, 1, 0, 2, 3, 2, 2, 1,1 };
+        int n = 11, k = 3;
+        LinkedHashMap<Integer, Integer> hm = new LinkedHashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (hm.containsKey(nums[i])) {
+                hm.put(nums[i], hm.get(nums[i]) + 1);
+            } else {
+                hm.put(nums[i], 1);
+            }
+        }
+        for (Integer x : hm.keySet()) {
+            if (hm.get(x) == k) {
+                ts.add(x);
+            }
+        }
+        int x = ts.first();
+        System.out.println(x);
+
+    }
+}
